6e0b79697aea5b833adf5d43338e752dfc421d33
Anthony <oduekene@gmail.com>
2024-05-21 19:53:39 -0300

    implement Spring MVC including regular validation and custom validation

--- src/main/java/com/example/springmvcdemo/controller/CustomerController.java
+++ src/main/java/com/example/springmvcdemo/controller/CustomerController.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CustomerController {
